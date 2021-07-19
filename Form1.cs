22a194a442f9cd6450bdc65abe09984d1b14c425
Joanna <aska3001@wp.pl>
2021-07-19 19:06:17 +0200

    before testing

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,35 @@ namespace MileageCalculator
 {
     public partial class Form1 : Form
     {
+        int startingMileage;
+        int endingMileage;
+
+        double milesTraveled;
+        double reimburseRate = 0.39;
+        double amountOwed;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startingMileage = (int)numericUpDown1.Value;
+            endingMileage = (int)numericUpDown2.Value;
+
+            if (startingMileage <= endingMileage)
+            {
+                milesTraveled = endingMileage -= startingMileage;
+                amountOwed = milesTraveled *= reimburseRate;
+                label4.Text = amountOwed.ToString() + " $";
+            }
+            else
+            {
+                MessageBox.Show("Starting milege has to be lower than ending mileage", "I cannot calculate the mileage");
+            }
+
         }
     }
 }
